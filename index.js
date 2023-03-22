8602eecafd6b0aceb9eeb3d7b004acb07a5a01d6
Victor Villazón <victorzn23@gmail.com>
2023-03-22 12:29:23 -0500

    feat: work with query params

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ app.get('/', (req, res) => {
   res.send('Hello from Express.js')
 })
 
+app.get('/products', (req, res) => {
+  const { limit, offset } = req.query
+
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.json({
+      message: 'No hay parámetros'
+    })
+  }
+
+  res.json([])
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+
+  res.json({ id })
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
 })
