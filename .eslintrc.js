8eec447a8e756aff0be7e79144417f56d718bb36
Victor Villazón <victorzn23@gmail.com>
2023-03-22 12:07:20 -0500

    chore: init project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  extends: 'standard',
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  rules: {
+    'no-console': 'warn'
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# leaning nodejs - My Store
+
+```
+npm i -DE eslint
+
+npx eslint --init
+```
