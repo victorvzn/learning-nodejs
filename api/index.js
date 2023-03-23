a5cecc7fa75aea9441b74d8579347bc7c7d924fd
Victor Villazón <victorzn23@gmail.com>
2023-03-23 12:56:55 -0500

    preparando para vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const routerApi = require('./routes')
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
@@ -9,11 +10,25 @@ const PORT = 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
+const whiteList = ['http://localhost:8080']
+
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed'))
+    }
+  }
+}
+
+app.get('/api', (req, res) => {
   res.send('Hello from Express.js')
 })
 
 routerApi(app)
+
+app.use(cors(options))
 
 // Custom middlewares
 app.use(logErrors)
