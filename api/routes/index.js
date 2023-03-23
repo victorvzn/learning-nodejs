53d9925d45f52c8a25fb00278b733a6f90a9c59b
Victor Villazón <victorzn23@gmail.com>
2023-03-23 16:11:31 -0500

    categories completed

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 //
 const productsRouter = require('./products.router')
-// const usersRouter = require('./users.router')
+const categoriesRouter = require('./categories.router')
 
 function routerApi (app) {
   const router = express.Router()
@@ -9,7 +9,7 @@ function routerApi (app) {
   app.use('/api/v1', router)
 
   router.use('/products', productsRouter)
-  // app.use('/users', usersRouter)
+  router.use('/categories', categoriesRouter)
 }
 
 module.exports = routerApi
